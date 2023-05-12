032106ef33abb15f3d206b422e7a2cdcd70250c0
Tom Beynon <tom@tombeynon.co.uk>
2023-05-12 16:11:14 +0100

    Redirect to Leap mobile app browser for mobile

--- src/utils/LeapSignerProvider.mjs
+++ src/utils/LeapSignerProvider.mjs
@@ -1,5 +1,8 @@
 import _ from 'lodash'
 import SignerProvider from "./SignerProvider.mjs"
+import {
+  isMobile,
+} from "@walletconnect/browser-utils";
 
 export default class LeapSignerProvider extends SignerProvider {
   key = 'leap'
@@ -7,7 +10,16 @@ export default class LeapSignerProvider extends SignerProvider {
   keychangeEvent = 'leap_keystorechange'
   suggestChainSupport = true
 
-  enable(network){
-    return super.enable(network)
+  async connect(network) {
+    if(this.provider){
+      return super.connect(network)
+    }else if(isMobile()){
+      window.location.href = 'https://leapcosmoswallet.page.link/HawhyWcCuygLbkvT6';
+      throw new Error('Please use the in-app browser to access REStake.')
+    }
+  }
+
+  available() {
+    return !!this.provider || isMobile()
   }
 }
